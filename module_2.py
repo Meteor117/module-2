96c6a6678700b86c2e1301d67871ff20ba2b6a53
Meteor117 <kharitonov_62@mail.ru>
2024-08-17 08:52:37 +0300

    Add files via upload

---
+++ module_2.py
@@ -0,0 +1,15 @@
+#module_2_2.py
+my_list=[42,69,13,0,99,-5,9,8,7,-6,5]
+index = 0
+while index<len(my_list):
+    if numbers [index]  > 0 :
+        print (f''{my_list[index]})
+else:
+index += 1
+#на 0 надо сделать континью или брейк
+    
+#y = [1,45,66,22,1988]
+#x = random.randrange(0, len(y))
+#while ( x not in y ):
+#   print("X = ",x)
+#x = random.randrange(0, 1999)
